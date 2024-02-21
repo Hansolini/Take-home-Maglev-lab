f69bd65e51eaff3a504f15368b29e11daade05a8
Sverre Graffer <svergr@stud.ntnu.no>
2024-02-21 13:48:42 +0100

    The description in the last commit is for this one. Forgot to add all the changes.

--- student_contributions/code/Observability_check/obs_plot.m
+++ student_contributions/code/Observability_check/obs_plot.m
@@ -29,31 +29,33 @@ clc
 
 no_input = zeros(4, 1);
 
+% Choose simulation time and epsilon used 
+% to calculate the observability gramian:
 T = 10e-3;
 epsilon = 1e-5;
 
+% Define grid size and the number of points in a row or column in the grid:
 grid_size = 15e-3;
-grid_resolution = 3;
+grid_points = 3;
 
+% Make sure that the grid includes the equilibrium:
 if round(mod(grid_resolution, 2)) == 0
-    disp("Please use an odd number such that the equilibrium is included in the grid.")
+    disp("Please use an odd number such that the equilibrium is included in the grid!")
     return
 end
 
+% Create grid:
+x = linspace(-grid_size, grid_size, grid_points);
+y = linspace(grid_size, -grid_size, grid_points);
 
-x = linspace(-grid_size, grid_size, grid_resolution);
-y = linspace(grid_size, -grid_size, grid_resolution);
-
-%lui
-%lecn
+% Run observability check for every point in the grid:
 lui = zeros(grid_resolution);
 lecn = zeros(grid_resolution);
 for row_index = 1:size(y, 2)
     for column_index = 1:size(x, 2)
         x0 = [x(column_index),y(row_index),zEq(1),zeros(1,9)]';
         [lui(row_index, column_index), lecn(row_index, column_index)] = obs_check(f, h, x0, no_input, T, epsilon);
-        %a(k, l) = k;
-        %b(k, l) = l;
+        %disp([row_index;column_index])
     end
 end
 
