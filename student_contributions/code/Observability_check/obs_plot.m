5d459801f74a569072256771cd89c91d5176f3e5
Hans Alvar Engmark <hans.a.engmark@ntnu.no>
2024-02-22 21:38:56 +0100

    Added additional update messages and saving of mat files

--- student_contributions/code/Observability_check/obs_plot.m
+++ student_contributions/code/Observability_check/obs_plot.m
@@ -37,7 +37,7 @@ states_to_include = [1:5];%[1:5 7:11];
 
 % Define grid size and the number of points in a row or column in the grid:
 grid_size = 30e-3;
-grid_points = 61;
+grid_points = 3;
 
 % Make sure that the grid includes the equilibrium:
 if round(mod(grid_points, 2)) == 0
@@ -52,24 +52,28 @@ y = linspace(grid_size, -grid_size, grid_points);
 % Run observability check for every point in the grid:
 lui = zeros(grid_points);
 lecn = zeros(grid_points);
-tic;
 timestamps = zeros(grid_points);
 wait = waitbar(0, sprintf("0 out of %d points done.", grid_points^2));
 for row_index = 1:size(y, 2)
     for column_index = 1:size(x, 2)
+        tic;
         x0 = [x(column_index),y(row_index),zEq(1),zeros(1,9)]';
         [lui(row_index, column_index), lecn(row_index, column_index)] = obs_check(f, h, x0, no_input, T, epsilon, states_to_include);
-        %disp([row_index;column_index])
         
         % Save timestamp:
         timestamps(row_index, column_index) = toc;
 
+        % Compute final time
+        seconds_left = mean(timestamps(1:row_index,1:column_index),'all')*sum(timestamps(:) == 0);
+        current_time = datetime('now');
+        final_time = currentTime + seconds(seconds_left);
+
         % Update waitbar:
         points_done = grid_points*(row_index - 1) + column_index;
-        waitbar(points_done/grid_points^2, wait,  sprintf("%d out of %d points done.", points_done, grid_points^2));
+        waitbar(points_done/grid_points^2, wait,  sprintf("%d out of %d points done.\n Final time: %s", points_done, grid_points^2, datestr(final_time)));
     end
+    save('obs_plot.mat')
 end
 pause(250e-3);
 close(wait);
-%% Plot
 
