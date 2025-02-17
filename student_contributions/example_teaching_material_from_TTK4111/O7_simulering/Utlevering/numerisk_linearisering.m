5fe4e5ab3e9de35c19eb504e745416cc0b883af0
Hans Alvar Engmark <hans.a.engmark@ntnu.no>
2025-02-17 11:24:48 +0100

    Added missing system dynamics functions to TTK4111 example

--- student_contributions/example_teaching_material_from_TTK4111/O7_simulering/Utlevering/numerisk_linearisering.m
+++ student_contributions/example_teaching_material_from_TTK4111/O7_simulering/Utlevering/numerisk_linearisering.m
@@ -59,3 +59,36 @@ u_eq = [0,0]';
 % 
 % HUSK å legge til simulatormappen i path: høyreklikk på "simulator" -> 
 % "add to path" -> "selected folders and subfolders"
+
+%% Helper Functions
+function dx = maglevSystemDynamics2d(x,u,params)
+    % ### HACK from 2d to 3d ###
+    x_full = [
+        x(1),0,x(2),0,x(3),0,...
+        x(4),0,x(5),0,x(6),0
+        ]';
+    u_full = [u(1), u(2), 0, 0]';
+    % ##########################
+    
+    dx = maglevSystemDynamics(x_full,u_full,params);
+    
+    % ### HACK from 3d to 2d ###
+    dx = dx([1,3,5,7,9,11]');
+    % ##########################
+end
+
+function y = maglevSystemMeasurements2d(x,params)
+    % ### HACK from 2d to 3d ###
+    x_full = [
+        x(1),0,x(2),0,x(3),0,...
+        x(4),0,x(5),0,x(6),0
+        ]';
+    u_full = [0, 0, 0, 0]';
+    % ##########################
+    
+    y = maglevSystemMeasurements(x_full,u_full,params);
+    
+    % ### HACK from 3d to 2d ###
+    y = y([1,3]');
+    % ##########################
+end
